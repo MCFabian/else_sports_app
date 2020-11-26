667c5b646a29168b6246d588b705c697e35e7c9f
Fabian Rauer <60275181+MCFabian@users.noreply.github.com>
2020-11-26 21:29:33 +0100

    Added notifications-menu

--- Home_Finder/assets/www/js/bb.js
+++ Home_Finder/assets/www/js/bb.js
@@ -11,19 +11,54 @@ function toggleMenu() {
 }
 
 window.addEventListener("load", function(){
-    document.getElementById("overlay").addEventListener("click", function(){
+/*     document.getElementById("overlay").addEventListener("click", function(){
         UD_MENU_OPEN = !UD_MENU_OPEN;
         document.getElementById("bb-btn").classList.toggle("is-active", UD_MENU_OPEN);
         document.getElementById("nav").classList.toggle("nav-active", UD_MENU_OPEN);
         document.getElementById("overlay").classList.toggle("overlay-active", UD_MENU_OPEN);
-    });
+    }); */
 
-    document.getElementById("overlay").addEventListener("touchstart", function(){
+/*     document.getElementById("overlay").addEventListener("touchstart", function(){
         UD_MENU_OPEN = !UD_MENU_OPEN;
         document.getElementById("bb-btn").classList.toggle("is-active", UD_MENU_OPEN);
         document.getElementById("nav").classList.toggle("nav-active", UD_MENU_OPEN);
         document.getElementById("overlay").classList.toggle("overlay-active", UD_MENU_OPEN);
-    });
+    }); */
 })
+
+
+window.addEventListener("load", function(){
+    //OPEN NOTIFICATIONS
+
+    var notification = document.getElementById("notifications");
+    var notificationcounter = document.getElementById("notificationcounter");
+    var notificationoverlay = document.getElementById("notification_overlay");
+    var notificationsclose = document.getElementById("notifications-close");
+    
+    notification.addEventListener("click", function(){  //CHANGE To TOUCHSTART
+        callnotifications();
+    })
+
+    notificationsclose.addEventListener("click", function(){  //CHANGE To TOUCHSTART
+        callnotifications();
+    })
+
+    function callnotifications(){
+        document.getElementById("overlay").classList.toggle("overlay-active");
+        notificationoverlay.classList.toggle("slide_overlay-active");
+
+    }
+
+    //CHECK QUANTITY OF NOTIFICATIONS
+    var notificationquantity = document.getElementById("notificationlist").childElementCount;
+    notificationcounter.innerHTML = (notificationquantity);
+
+
+
+
+
+})
+
+
 
 
